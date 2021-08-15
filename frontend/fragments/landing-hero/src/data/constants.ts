687fee82a592a5475360e86003d810f7b7780c6e
Nikita Elfimov <ne@atls.team>
2021-08-15 17:12:15 +0500

    chore(common): switch env

--- frontend/fragments/landing-hero/src/data/constants.ts
+++ frontend/fragments/landing-hero/src/data/constants.ts
@@ -1,12 +1,15 @@
 /** Local * */
 
-export const HERO_CONTENT_EN = 'cG9zdDo0ODM='
+// export const HERO_CONTENT_EN = 'cG9zdDo0ODM='
+// export const HERO_CONTENT_RU = 'cG9zdDo0ODE='
+//
+// export const VIDEO_DESKTOP = 'cG9zdDo0NDA='
+// export const VIDEO_MOBILE = 'cG9zdDo0NDY='
+
+/** Prod * */
+
+export const HERO_CONTENT_EN = 'cG9zdDo0OTQ='
 export const HERO_CONTENT_RU = 'cG9zdDo0ODE='
 
 export const VIDEO_DESKTOP = 'cG9zdDo0NDA='
 export const VIDEO_MOBILE = 'cG9zdDo0NDY='
-
-/** Prod * */
-
-// export const HERO_CONTENT_EN = 'cG9zdDo0OTQ='
-// export const HERO_CONTENT_RU = 'cG9zdDo0ODE='
--- frontend/landing/app/src/.next/static/chunks/webpack.js
+++ frontend/landing/app/src/.next/static/chunks/webpack.js
@@ -182,7 +182,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "1bd51512a1cdf8a68f4a"; }
+/******/ 		__webpack_require__.h = function() { return "ead41ec5bcb1975c9060"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
