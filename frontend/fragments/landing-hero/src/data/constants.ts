b42a4052c74f9caa046517691882c5ed0f963534
Nikita Elfimov <ne@atls.team>
2021-08-21 19:31:32 +0500

    feat(fragments): add slider inside popover

--- frontend/fragments/landing-hero/src/data/constants.ts
+++ frontend/fragments/landing-hero/src/data/constants.ts
@@ -1,17 +1,17 @@
 /** Local * */
 
-// export const HERO_CONTENT_EN = 'cG9zdDo0ODM='
-// export const HERO_CONTENT_RU = 'cG9zdDo0ODE='
-//
-// export const VIDEO_DESKTOP = 'cG9zdDo0NDA='
-// export const VIDEO_MOBILE = 'cG9zdDo0NDY='
-// export const VIDEO_MOBILE_2 = 'cG9zdDo2Mzc='
-
-/** Prod * */
-
-export const HERO_CONTENT_EN = 'cG9zdDo0OTQ='
+export const HERO_CONTENT_EN = 'cG9zdDo0ODM='
 export const HERO_CONTENT_RU = 'cG9zdDo0ODE='
 
 export const VIDEO_DESKTOP = 'cG9zdDo0NDA='
 export const VIDEO_MOBILE = 'cG9zdDo0NDY='
-export const VIDEO_MOBILE_2 = 'cG9zdDo3NjQ='
+export const VIDEO_MOBILE_2 = 'cG9zdDo2Mzc='
+
+/** Prod * */
+
+// export const HERO_CONTENT_EN = 'cG9zdDo0OTQ='
+// export const HERO_CONTENT_RU = 'cG9zdDo0ODE='
+//
+// export const VIDEO_DESKTOP = 'cG9zdDo0NDA='
+// export const VIDEO_MOBILE = 'cG9zdDo0NDY='
+// export const VIDEO_MOBILE_2 = 'cG9zdDo3NjQ='
--- frontend/fragments/landing-reviews/src/landing-reviews.component.tsx
+++ frontend/fragments/landing-reviews/src/landing-reviews.component.tsx
@@ -1,5 +1,6 @@
 import React                   from 'react'
 import { FC }                  from 'react'
+import { useState }            from 'react'
 
 import { Button }              from '@ui/button'
 import { useCarousel }         from '@ui/carousel'
@@ -13,53 +14,64 @@ import { ArrowRightIcon }      from '@ui/icons'
 
 import { messages }            from './messages'
 import { Item }                from './item'
+import { Popover }             from './popover'
 import { useData }             from './data'
 import { LandingReviewsProps } from './landing-reviews.interface'
 
 const LandingReviews: FC<LandingReviewsProps> = ({ language }) => {
-  const reviews = useData()
   const [swipeLeft, swipeRight, Carousel] = useCarousel()
+  const [activeItem, setActiveItem] = useState<number | null>(null)
 
   const filterByLanguage = ({ language: { code } }) => code === language
 
+  const reviews = useData().filter(filterByLanguage)
+
   return (
-    <Box
-      px={['32px', '32px', '0px']}
-      backgroundColor='background.lightGray'
-      justifyContent='center'
-    >
-      <Layout width='100%' maxWidth={1280}>
-        <Column justifyContent='center' width='100%'>
-          <Layout flexBasis={120} />
-          <Row alignItems='center'>
-            <Text color='text.black' fontWeight='slim' fontSize='massive'>
-              {messages.reviews[language]}
-            </Text>
-            <Layout flexGrow={1} flexBasis={[64, 64, 0]} />
-            <Layout width={128}>
-              <Button colors='transparent' width={44} onClick={swipeLeft}>
-                <Layout>
-                  <ArrowLeftIcon width={8} height={16} />
-                </Layout>
-              </Button>
-              <Layout flexBasis={16} />
-              <Button colors='transparent' width={44} onClick={swipeRight}>
-                <Layout>
-                  <ArrowRightIcon width={8} height={16} />
-                </Layout>
-              </Button>
-            </Layout>
-          </Row>
-          <Layout flexBasis={48} />
-          <Carousel spaceBetween={32}>
-            {reviews.filter(filterByLanguage).map((review) => (
-              <Item review={review} language={language} />
-            ))}
-          </Carousel>
-          <Layout flexBasis={120} />
-        </Column>
-      </Layout>
-    </Box>
+    <>
+      <Popover
+        visible={activeItem}
+        setVisible={setActiveItem}
+        reviews={reviews}
+        language={language}
+      />
+      <Box
+        px={['32px', '32px', '0px']}
+        backgroundColor='background.lightGray'
+        justifyContent='center'
+      >
+        <Layout width='100%' maxWidth={1280}>
+          <Column justifyContent='center' width='100%'>
+            <Layout flexBasis={120} />
+            <Row alignItems='center'>
+              <Text color='text.black' fontWeight='slim' fontSize='massive'>
+                {messages.reviews[language]}
+              </Text>
+              <Layout flexGrow={1} flexBasis={[64, 64, 0]} />
+              <Layout width={128}>
+                <Button colors='transparent' width={44} onClick={swipeLeft}>
+                  <Layout>
+                    <ArrowLeftIcon width={8} height={16} />
+                  </Layout>
+                </Button>
+                <Layout flexBasis={16} />
+                <Button colors='transparent' width={44} onClick={swipeRight}>
+                  <Layout>
+                    <ArrowRightIcon width={8} height={16} />
+                  </Layout>
+                </Button>
+              </Layout>
+            </Row>
+            <Layout flexBasis={48} />
+            <Carousel spaceBetween={32}>
+              {reviews.map((review, index) => (
+                <Item review={review} language={language} onClick={() => setActiveItem(index)} />
+              ))}
+            </Carousel>
+            <Layout flexBasis={120} />
+          </Column>
+        </Layout>
+      </Box>
+    </>
   )
 }
 
