e7827443bac8741c024ae0963d6074f6c1096713
Nikita Elfimov <ne@atls.team>
2021-09-16 19:57:37 +0500

    chore: change copy button

--- frontend/fragments/landing-contacts/src/landing-contacts.component.tsx
+++ frontend/fragments/landing-contacts/src/landing-contacts.component.tsx
@@ -1,11 +1,7 @@
 import React                    from 'react'
 import { FC }                   from 'react'
-import { useState }             from 'react'
-import { motion }               from 'framer-motion'
-import { useAnimation }         from 'framer-motion'
 
-import { Button }               from '@ui/button'
-import { Condition }            from '@ui/condition'
+import { Copy }                 from '@ui/copy'
 import { Layout }               from '@ui/layout'
 import { Row }                  from '@ui/layout'
 import { Column }               from '@ui/layout'
@@ -13,7 +9,6 @@ import { Box }                  from '@ui/layout'
 import { Link }                 from '@ui/link'
 import { Image }                from '@ui/image'
 import { Text }                 from '@ui/text'
-import { CopyIcon }             from '@ui/icons'
 import { Form }                 from '@ui/form'
 
 import { useData }              from './data'
@@ -22,60 +17,6 @@ import { LandingContactsProps } from './landing-contacts.interface'
 
 const LandingContacts: FC<LandingContactsProps> = ({ language }) => {
   const { contacts, feedbackEmail, feedbackPhone, asset, workingHours } = useData()
-  const [email, setEmail] = useState<boolean>(false)
-  const [phone, setPhone] = useState<boolean>(false)
-
-  const copy = (content) => {
-    if (navigator && navigator.clipboard) navigator.clipboard.writeText(content)
-    else {
-      const input = document.createElement('input')
-      input.setAttribute('type', 'text')
-      input.style.position = 'absolute'
-      input.style.left = '-100%'
-      input.setAttribute('value', content)
-      document.body.appendChild(input)
-      input.select()
-      document.execCommand('copy')
-      document.body.removeChild(input)
-    }
-  }
-
-  const Copied = ({ target, setTarget }) => {
-    const main = useAnimation()
-
-    main
-      .start({
-        opacity: 1,
-      })
-      .then(() => {
-        setTimeout(() => {
-          main
-            .start({
-              opacity: 0,
-            })
-            .then(() => {
-              setTarget(false)
-            })
-        }, 1000)
-      })
-
-    return (
-      <Condition match={target}>
-        <motion.div initial={{ opacity: 0 }} animate={main}>
-          <Box
-            px='10px'
-            py='6px'
-            backgroundColor='background.transparentBlack'
-            borderRadius='small'
-          >
-            <Text fontSize='semiMedium' color='text.white'>
-              {messages.copied[language]}
-            </Text>
-          </Box>
-        </motion.div>
-      </Condition>
-    )
-  }
 
   return (
     <Box
@@ -130,22 +71,9 @@ const LandingContacts: FC<LandingContactsProps> = ({ language }) => {
                     <Layout flexBasis={16} />
                     <Layout display={['none', 'none', 'flex']}>
                       <Box width={40} height={40}>
-                        <Button
-                          width='100%'
-                          colors='clipboard'
-                          onClick={() => {
-                            copy(feedbackEmail.content)
-                            setEmail(true)
-                          }}
-                        >
-                          <Layout>
-                            <CopyIcon width={15} height={18} />
-                          </Layout>
-                        </Button>
+                        <Copy content={feedbackEmail.content} />
                       </Box>
                     </Layout>
-                    <Layout flexBasis={10} />
-                    <Copied target={email} setTarget={setEmail} />
                   </Row>
                   <Layout flexBasis={32} />
                   <Row alignItems='center'>
@@ -163,23 +91,8 @@ const LandingContacts: FC<LandingContactsProps> = ({ language }) => {
                     </Layout>
                     <Layout flexBasis={16} />
                     <Layout display={['none', 'none', 'flex']}>
-                      <Box width={40} height={40}>
-                        <Button
-                          width='100%'
-                          colors='clipboard'
-                          onClick={() => {
-                            copy(feedbackPhone.content)
-                            setPhone(true)
-                          }}
-                        >
-                          <Layout>
-                            <CopyIcon width={15} height={18} />
-                          </Layout>
-                        </Button>
-                      </Box>
+                      <Copy content={feedbackPhone.content} />
                     </Layout>
-                    <Layout flexBasis={10} />
-                    <Copied target={phone} setTarget={setPhone} />
                   </Row>
                   <Layout flexBasis={16} />
                   <Layout>
