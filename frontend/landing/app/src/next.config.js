a877cea2bbe48fa0c41ffae318874a5a43628a83
Nikita Elfimov <ne@atls.team>
2022-01-03 17:00:43 +0500

    chore(common): remove wp theme integration

--- frontend/landing/app/src/next.config.js
+++ frontend/landing/app/src/next.config.js
@@ -3,26 +3,18 @@ const { withAliases } = require('@providers/next-config-with-aliases')
 const { withFutureWebpack5 } = require('@providers/next-config-with-future-webpack-5')
 const withPlugins = require('next-compose-plugins')
 
-module.exports = withPlugins(
-  [
-    withWorkspaces,
-    withFutureWebpack5,
-    withAliases(
-      [
-        '@apollo/client$',
-        '@apollo/client/react/ssr$',
-        '@apollo/client/core$',
-        'events',
-        '@emotion/react',
-        '@emotion/styled',
-      ],
-      require
-    ),
-  ],
-  {
-    basePath:
-      process.env.NODE_ENV === 'production'
-        ? process.env.BASE_PATH || '/wp-content/themes/dream-team/dist'
-        : '',
-  }
-)
+module.exports = withPlugins([
+  withWorkspaces,
+  withFutureWebpack5,
+  withAliases(
+    [
+      '@apollo/client$',
+      '@apollo/client/react/ssr$',
+      '@apollo/client/core$',
+      'events',
+      '@emotion/react',
+      '@emotion/styled',
+    ],
+    require
+  ),
+])
