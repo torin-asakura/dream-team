37f82a3f41653da9efb1584fe968c8c511fc6a20
Nikita Elfimov <me@tfk.name>
2022-07-04 21:38:32 +0500

    feat(app): reinit with icons

--- landing/app/src/pages/_document.tsx
+++ landing/app/src/pages/_document.tsx
@@ -1,10 +1,36 @@
 import { withGtag }      from '@atls/next-document-with-gtag'
 import { withHelmet }    from '@atls/next-document-with-helmet'
-import { withIcons }     from '@atls/next-document-with-icons'
 import { withOpenGraph } from '@atls/next-document-with-opengraph'
 
 import Document          from 'next/document'
+import React             from 'react'
 import compose           from 'recompose/compose'
+
+const withIcons = () => (TargetComponent) =>
+  class WithIcons extends TargetComponent {
+    static async getInitialProps(context) {
+      const props = await super.getInitialProps(context)
+
+      props.head.push(
+        <link
+          rel='apple-touch-icon'
+          href='https://wp.dream-team.tech/wp-content/uploads/2022/07/apple_touch_icon.png'
+        />,
+        <link
+          rel='icon'
+          type='image/png'
+          href='https://wp.dream-team.tech/wp-content/uploads/2022/07/favicon.png'
+        />
+      )
+
+      return props
+    }
+
+    static renderDocument(...args) {
+      // @ts-ignore
+      return Document.renderDocument(...args)
+    }
+  }
 
 const withProviders = compose(
   withOpenGraph({ image: 'https://dream-team.tech/wp-content/uploads/2021/07/cover.jpg' }),
