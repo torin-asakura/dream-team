6fe12ae0a1c6cb9d3f0ef88f82e8ff6a8d3877c0
Nikita Elfimov <me@tfk.name>
2022-07-13 23:49:56 +0500

    chore(app): cache headers

--- landing/app/src/next.config.js
+++ landing/app/src/next.config.js
@@ -6,4 +6,35 @@ module.exports = {
     outputStandalone: true,
     esmExternals: 'loose',
   },
+  async headers() {
+    return [
+      {
+        source: '/',
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'public, max-age=86400, must-revalidate',
+          },
+        ],
+      },
+      {
+        source: '/contacts',
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'public, max-age=86400, must-revalidate',
+          },
+        ],
+      },
+      {
+        source: '/privacy-policy',
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'public, max-age=86400, must-revalidate',
+          },
+        ],
+      },
+    ]
+  },
 }
