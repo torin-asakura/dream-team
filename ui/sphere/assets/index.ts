75264727bdf324220e3c4feb294041b06aa8cf77
Dmitry <dimafomin02@gmail.com>
2024-01-31 22:35:01 +0300

    feat: added models to assets

--- ui/sphere/assets/index.ts
+++ ui/sphere/assets/index.ts
@@ -1,4 +1,15 @@
-import SphereBin  from './sphere_data.bin'
-import SphereGltf from './sphere.gltf'
+import OldSphereBin          from './old-sphere_data.bin'
+import OldSphereGltf         from './old-sphere.gltf'
+import SphereBin             from './sphere_data.bin'
+import SphereNoAnimationBin  from './sphere-no-animation_data.bin'
+import SphereNoAnimationGltf from './sphere-no-animation.gltf'
+import SphereGltf            from './sphere.gltf'
 
-export { SphereGltf, SphereBin }
+export {
+  OldSphereGltf,
+  OldSphereBin,
+  SphereBin,
+  SphereGltf,
+  SphereNoAnimationBin,
+  SphereNoAnimationGltf,
+}
