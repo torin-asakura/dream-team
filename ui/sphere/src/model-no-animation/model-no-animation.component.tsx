36ed72c1f8e47f022a5950dfa762b3e98e6287be
Dmitry <dimafomin02@gmail.com>
2024-01-31 22:35:51 +0300

    feat: added canvas-inner component

---
+++ ui/sphere/src/model-no-animation/model-no-animation.component.tsx
@@ -0,0 +1,62 @@
+import { useGLTF }               from '@react-three/drei'
+
+import React                     from 'react'
+import { FC }                    from 'react'
+import { Group }                 from 'three'
+import { useRef }                from 'react'
+
+import { SphereNoAnimationGltf } from '../../assets'
+import { GLTFResult }            from './model-no-animation.interfaces'
+import { ModelProps }            from './model-no-animation.interfaces'
+
+export const ModelNoAnimation: FC<ModelProps> = (props) => {
+  const group = useRef<Group>(null)
+  const { nodes, materials } = useGLTF(SphereNoAnimationGltf, true) as unknown as GLTFResult
+
+  return (
+    <group ref={group} {...props} dispose={null}>
+      <group name='Scene'>
+        <mesh
+          name='Sphere'
+          castShadow
+          receiveShadow
+          geometry={nodes.Sphere.geometry}
+          material={materials['Material.001']}
+          rotation={[1.079, -0.013, 0.085]}
+        />
+        <mesh
+          name='Sphere001'
+          castShadow
+          receiveShadow
+          geometry={nodes.Sphere001.geometry}
+          material={materials['Material.002']}
+          scale={0.964}
+        />
+        <group
+          name='Area'
+          position={[5.793, 6.449, 8.961]}
+          rotation={[0, -0.996, -0.999]}
+          scale={[-11.858, 11.858, 11.858]}
+        />
+        <group
+          name='Area001'
+          position={[3.488, 6.267, -6.024]}
+          rotation={[-3.096, -0.988, -2.081]}
+          scale={[-11.858, 11.858, 11.858]}
+        />
+        <group
+          name='Area002'
+          position={[7.068, -7.785, 1.371]}
+          rotation={[1.591, -0.777, -1.55]}
+          scale={[-11.858, 11.858, 11.858]}
+        />
+        <group
+          name='Area003'
+          position={[11.711, -3.164, -0.411]}
+          rotation={[1.586, -0.301, -1.56]}
+          scale={[-11.858, 11.858, 11.858]}
+        />
+      </group>
+    </group>
+  )
+}
--- ui/sphere/src/sphere.component.tsx
+++ ui/sphere/src/sphere.component.tsx
@@ -1,23 +1,22 @@
-import { Canvas }      from '@react-three/fiber'
-
 import React           from 'react'
 import { lazy }        from 'react'
 import { memo }        from 'react'
 
+import { CanvasInner } from './canvas-inner'
 import { Loader }      from './loader'
 import { SphereProps } from './sphere.interfaces'
 
 const ModelComponent = lazy(() => import('./model').then(({ Model }) => ({ default: Model })))
+const ModelNoAnimationComponent = lazy(() =>
+  import('./model-no-animation').then(({ ModelNoAnimation }) => ({ default: ModelNoAnimation })))
 
 export const Sphere = memo<SphereProps>(({ style }) => (
-  <Canvas style={style} camera={{ position: [10, 0, 0], fov: 30 }}>
-    <ambientLight color='#ffffff' intensity={0.1} />
-    <directionalLight color='#ffffff' position={[10, 4, -2]} intensity={2.5} />
-    <directionalLight color='#10EFFF' position={[1, 6.5, -9]} intensity={2.5} />
-    <directionalLight color='#e261ff' position={[0, -2, 3]} intensity={2.5} />
-    <directionalLight color='#ffffff' position={[0, -10, 0]} intensity={0.4} />
+  <CanvasInner style={style}>
     <Loader>
       <ModelComponent />
     </Loader>
-  </Canvas>
+    <Loader>
+      <ModelNoAnimationComponent />
+    </Loader>
+  </CanvasInner>
 ))
